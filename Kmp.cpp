2f6d87f8af0daacb5466bc2d0059fa15c394ea7b
sunjie <sunjieee@mail.ustc.edu.cn>
2014-12-17 22:12:43 +0800

    change format

--- Kmp.cpp
+++ Kmp.cpp
@@ -1,37 +1,37 @@
-void getFail(char *P, int *f)
+void getFail(char* P, int* f)
 {
-    int m=strlen(P);
-    f[0]=0;
-    f[1]=0;
-    for(int i=1;i<m;i++)
+    int m = strlen(P);
+    f[0] = 0;
+    f[1] = 0;
+    for (int i = 1 ; i < m; i++)
     {
-        int j=f[i];
-        while(j&&P[i]!=P[j])
+        int j = f[i];
+        while (j && P[i] != P[j])
         {
-            j=f[j];
+            j = f[j];
         }
-        f[i+1]=P[i]==P[j]?j+1:0;
+        f[i+1] = P[i] == P[j] ? j+1 : 0;
     }
 }
-void find(char *T,char *P,int *f)
+void find(char* T, char* P, int* f)
 {
-    int n=strlen(T);
-    int m=strlen(P);
-    getFail(P,f);
-    int j=0;
-    for(int i=0;i<n;i++)
+    int n = strlen(T);
+    int m = strlen(P);
+    getFail(P, f);
+    int j = 0;
+    for (int i = 0; i < n; i++)
     {
-        while(j&&P[j]!=T[i])
+        while (j && P[j] != T[i])
         {
-            j=f[j];
+            j = f[j];
         }
-        if(P[j]==T[i])
+        if (P[j] == T[i])
         {
             j++;
         }
-        if(j==m)
+        if(j == m)
         {
-            printf("%d\n",i-m+1);
+            printf("%d\n", i - m + 1);
         }
     }
 }
